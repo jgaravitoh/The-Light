05beb523b895b8c80058678ef18b3ef2efaff224
Juan Garavito <jgaravitoh@unal.edu.co>
2025-05-01 16:43:33 -0500

    feat: ✨ almost finished CSV dialogue system

--- Assets/Scripts/DialogueSystemV2.0/FirstTestDialogues_v2.cs
+++ Assets/Scripts/DialogueSystemV2.0/FirstTestDialogues_v2.cs
@@ -1,30 +1,21 @@
-using System;
 using UnityEngine;
 
 public class FirstTestDialogues_v2 : MonoBehaviour
 {
+    public DialogueSystemManager dialogueManager;
+    private bool dialogueStarted = false;
+
     void Start()
     {
-        var service = new DialogueCSVDataService();
-        service.LoadDataAsync<DialogueTable>("Assets/Dialogue/DialoguesTest1.csv", false, (table) =>
-        {
-            if (table == null)
-            {
-                Debug.LogError("Could not load dialogue.");
-                return;
-            }
-        for(int i=0; i<table.Dialogues.Length; i++) {
-                if (table.ImageNames[i] == null)
-                {
-                    Debug.Log($"[{table.Ids[i]}] [{table.Separators[i]}] image: [null] {table.CharacterNames[i]} says: \"{table.Dialogues[i]}\"");
-                }
-                else
-                {
-                    Debug.Log($"[{table.Ids[i]}] [{table.Separators[i]}] image: [{table.ImageNames[i]}] {table.CharacterNames[i]} says: \"{table.Dialogues[i]}\"");
+        dialogueManager = GameObject.Find("DialogueManagerV2").GetComponent<DialogueSystemManager>();
+    }
 
-                }
-            }
-            
-        });
+    void Update()
+    {
+        if (!dialogueStarted && dialogueManager.dialogueTable != null)
+        {
+            dialogueManager.LoadDialogue(20);
+            dialogueStarted = true;
+        }
     }
 }
