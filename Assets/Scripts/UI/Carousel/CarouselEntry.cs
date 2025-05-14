089e66f1032cad9fa366ffe6a13343f2014bb725
Juan Garavito <jgaravitoh@unal.edu.co>
2025-05-14 13:00:49 -0500

    feat: ✨ Added carousel for 2d ui elements, this commit also includes minimal changes to the dialogue system

---
+++ Assets/Scripts/UI/Carousel/CarouselEntry.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "New Carousel Entry", menuName = "UI/Carousel Entry",order =0)]
+public class CarouselEntry:ScriptableObject
+{
+    //[field: SerializeField] public Sprite EntryGraphic { get; private set; }
+    [field: SerializeField] public Sprite EntryGraphic { get; private set; }
+    [field: SerializeField] public string Headline { get; private set; }
+    [field: SerializeField, Multiline(10)] public string Description { get; private set; }
+
+    //[Header("Interaction")]
+    //[SerializeField] private string levelNameToLoad;
+
+    public void Interact()
+    {
+        //SceneManager.LoadScene(levelNameToLoad);         //add code here for button interaction if needed.
+    }
+}
